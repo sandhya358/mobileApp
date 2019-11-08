e21ae8d5fc4105066d54c97f00be38d7ebbcb57c
sandhya358 <tsandhya95@gmail.com>
2019-11-08 12:00:54 +0530

    code styling fixed

--- src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -18,46 +18,47 @@ import com.appsdeveloperblog.app.ws.service.UserService;
 import com.appsdeveloperblog.app.ws.shared.Utils;
 import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
 
-
 @Service
 public class UserServiceImpl implements UserService {
-
+	
+	
 	@Autowired
 	UserRepository userRepository;
-@Autowired
-Utils utils;
-@Autowired
-BCryptPasswordEncoder bCryptPasswordEncoder;
+	@Autowired
+	Utils utils;
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+
 	/**
-	 * @UserDto is user entity comes from the controller
-	 * Method to create the user in database
+	 * @UserDto is user entity comes from the controller Method to create the user
+	 *          in database
 	 */
 	@Override
-	public UserDto createUser(UserDto user)  {
-		 if(userRepository.findByEmail(user.getEmail()) != null) throw new RuntimeException("record already exists");//duplicate rows
-	
+	public UserDto createUser(UserDto user) {
+		if (userRepository.findByEmail(user.getEmail()) != null)
+			throw new RuntimeException("record already exists");// duplicate rows
+
 		UserEntity userEntity = new UserEntity();
 		BeanUtils.copyProperties(user, userEntity);
 
-String publicUserId=utils.generateUserId(30);
-userEntity.setUserId(publicUserId);// create public userid
-userEntity.setEncryptedpassword(bCryptPasswordEncoder.encode(user.getPassword()));//encode password
-	UserEntity storedUserDetails = userRepository.save(userEntity);
-	
+		String publicUserId = utils.generateUserId(30);
+		userEntity.setUserId(publicUserId);// create public userid
+		userEntity.setEncryptedpassword(bCryptPasswordEncoder.encode(user.getPassword()));// encode password
+		UserEntity storedUserDetails = userRepository.save(userEntity);
+
 		UserDto returnValue = new UserDto();
 
 		BeanUtils.copyProperties(storedUserDetails, returnValue);
-		
-		System.out.println("Value form the service layer " +returnValue.getUserId());
+
+		System.out.println("Value form the service layer " + returnValue.getUserId());
 
 		return returnValue;
-		}
+	}
+
 	@Override
 	public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
 		// TODO Auto-generated method stub
 		return null;
 	}
-		
-		
-	}
 
+}
